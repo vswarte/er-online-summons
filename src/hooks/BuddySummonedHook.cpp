4f08c8daf71461ff87b386bb0401a03214e099de
Vincent <vincent@vinsert.nl>
2022-08-29 17:44:16 +0200

    Bordered off some more log statements with the debug flag

--- src/hooks/BuddySummonedHook.cpp
+++ src/hooks/BuddySummonedHook.cpp
@@ -10,16 +10,24 @@ namespace EROnlineSummons {
     }
 
     bool BuddySummonedHook::onInvoke(uintptr_t worldChrMan, int buddyGoodsId, bool local) {
+        #ifndef NDEBUG
+        Logging::WriteLine("Invoked BuddySummonedHook: %i", buddyGoodsId);
+        #endif
+
         uintptr_t (*originalFunction)(...);
         *(uintptr_t*)&originalFunction = _instance->GetOriginal();
         auto result = originalFunction(worldChrMan, buddyGoodsId);
 
         // TODO: keep track of who initiated summons some other way as this is abusing registers that aren't ours
         if (local) {
+            #ifndef NDEBUG
             Logging::WriteLine("Invocation was local");
+            #endif
             _instance->_summonNetworking->SendSummonSpawned(buddyGoodsId);
         } else {
+            #ifndef NDEBUG
             Logging::WriteLine("Invocation was remote");
+            #endif
         }
 
         return result;
